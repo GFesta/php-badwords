e65c270eecd0d4adcd71791583e6c5f732919ce6
Giuseppe <festagiuseppe89@gmail.com>
2021-04-23 10:52:55 +0200

    correzione

--- index.php
+++ index.php
@@ -8,22 +8,21 @@
 <?php
     $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+    $badWord = $_GET['badword'];
     $badWordReplace = str_replace($badWord, "***", $paragraph);
-    $badWord = $_GET['Lorem'];
+    
 
 ?>
 
         <div>
             <h1>Lunghezza paragrafo:  <?php echo strlen($paragraph) . " " . "caratteri"?> </h1>
-            <p> <?php echo $paragraph //stampa del paragrafo in pagina?> </p>
+            <p> <?php echo $paragraph;?> </p>
         </div>
 
         <div>
-            <h1>Lunghezza del nuovo paragrafo: <?php echo strlen($paragraph) . " " . "caratteri" ?> </h1>
-            <p> <?php echo str_replace("Lorem", "* * *", $paragraph);?> </p>
+            <h1>Lunghezza del nuovo paragrafo: <?php echo strlen($badWordReplace) . " " . "caratteri" ?> </h1>
+            <p> <?php echo $badWordReplace;?> </p>
         </div>
-
-
 
     </body>
 </html>
